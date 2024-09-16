3ab889e3bef42e1b94cf584243fa76647549e62a
pGOD1000 <benitak58@gmail.com>
2024-09-16 18:04:45 +0100

    Add files via upload

---
+++ src.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("I am a chess engine made by pGOD1000");
+    return 0;
+}
---
+++ src.py
@@ -0,0 +1,8 @@
+import chess as ch
+import board as br
+
+def __init__(self, board, depth):
+    self.board=board
+    self.depth=depth
+
+if 
