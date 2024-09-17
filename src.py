5fd8d49c138daf8c9b39aaef3eacde6a090d672d
pGOD1000 <benitak58@gmail.com>
2024-09-17 18:25:54 +0100

    Add files via upload

--- src.py
+++ src.py
@@ -1,8 +1,20 @@
 import chess as ch
 import board as br
 
-def __init__(self, board, depth):
+class Pbot:
+
+
+ def __init__(self, board, depth, enpassant, promotion, capture, check):
     self.board=board
     self.depth=depth
+    self.enpassant=enpassant
+    self.promotion=promotion
+    self.capture=capture
+    self.check=check
 
-if 
+ def bestmove(self):
+   return self.depth(None, 1)
+ 
+ def evalFunction(self):
+   compt = 0
+   
